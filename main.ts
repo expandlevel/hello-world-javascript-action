3e376971ed64417bafeed3ba4412d9340dcfa6b4
Mahdi Sheibak <mahdi.zoraghi.sheibak@gmail.com>
2026-04-26 03:51:52 +0330

    download mp4

--- main.ts
+++ main.ts
@@ -3,8 +3,7 @@ import DownloadManager from "bun-downloader-manager";
 console.log({ DownloadManager });
 
 const urls = [
-  // "https://www.wow.xxx/get_file/8512/b98568b646dbe994a8239a96d06272de4256acbadf/93814000/93814514/93814514_720m.mp4/?download=true&download_filename=joshua-lewis-pays-gracey-snow-for-her-pussy-after-seeing-her-tits_720m.mp4",
-  // "https://dl.rozmusic.com/Music/1404/11/29/Ali%20Zand%20Vakili%20-%20Bekhaab%20Aroom.mp3",
+  "https://www.wow.xxx/get_file/8512/b98568b646dbe994a8239a96d06272de4256acbadf/93814000/93814514/93814514_720m.mp4/?download=true&download_filename=joshua-lewis-pays-gracey-snow-for-her-pussy-after-seeing-her-tits_720m.mp4",
 ];
 
 const downloadManager = new DownloadManager({
