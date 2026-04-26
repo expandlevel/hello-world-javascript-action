3a4d6fbdac4cbdeae6cbab43cbe01bd1eed77c77
Mahdi Sheibak <mahdi.zoraghi.sheibak@gmail.com>
2026-04-26 03:55:39 +0330

    download mp4 2

--- main.ts
+++ main.ts
@@ -2,8 +2,13 @@ import DownloadManager from "bun-downloader-manager";
 
 console.log({ DownloadManager });
 
-const urls = [
+const fileUrl = await fetch(
   "https://www.wow.xxx/get_file/8512/b98568b646dbe994a8239a96d06272de4256acbadf/93814000/93814514/93814514_720m.mp4/?download=true&download_filename=joshua-lewis-pays-gracey-snow-for-her-pussy-after-seeing-her-tits_720m.mp4",
+);
+
+const urls = [
+  // "https://www.wow.xxx/get_file/8512/b98568b646dbe994a8239a96d06272de4256acbadf/93814000/93814514/93814514_720m.mp4/?download=true&download_filename=joshua-lewis-pays-gracey-snow-for-her-pussy-after-seeing-her-tits_720m.mp4",
+  fileUrl.url,
 ];
 
 const downloadManager = new DownloadManager({
