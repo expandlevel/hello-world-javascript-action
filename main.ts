762d0a5039891b286f4a8729883dc4d674dd9c0b
Mahdi Sheibak <mahdi.zoraghi.sheibak@gmail.com>
2026-04-26 03:41:27 +0330

    add url to download

--- main.ts
+++ main.ts
@@ -1,5 +1,18 @@
+import DownloadManager from "bun-downloader-manager";
+
 console.log("==================");
 
-console.log("TEST ROOT");
+console.log({ DownloadManager });
+
+const urls = [
+  "https://www.wow.xxx/get_file/8512/b98568b646dbe994a8239a96d06272de4256acbadf/93814000/93814514/93814514_720m.mp4/?download=true&download_filename=joshua-lewis-pays-gracey-snow-for-her-pussy-after-seeing-her-tits_720m.mp4",
+];
+
+const downloadManager = new DownloadManager({
+  consoleLog: true, // Enable logging
+  method: "queue", // Use queue-based download
+});
+
+await downloadManager.download(urls);
 
 console.log("==================");
